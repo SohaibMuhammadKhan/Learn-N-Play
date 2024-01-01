dbfc0664d77bb8f6fea554be9ce628158b8ad32e
Sohaib <sohaib271201@gmail.com>
2024-01-01 19:58:10 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/learn_n_play/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_n_play
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/User Registration.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RegistrationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Register')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Registration form components go here
+            // Include fields for child's name, parent's email, etc.
+            // Use TextFormFields, RaisedButton, etc.
+          ],
+        ),
+      ),
+    );
+  }
+}
